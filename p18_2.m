0627a1e2d54cdfb2134f49307e3057684ea16980
Shivansh Rai <shivansh@freebsd.org>
2019-03-28 23:33:59 +0530

    Implement differentiation via Chebychev matrices and FFT

---
+++ p18_2.m
@@ -0,0 +1,31 @@
+% p18_2.m - accuracy of FFT spectral differentiation
+% The functions in this program are the same as the ones in p12.m
+
+tic;
+Nmax = 50; E = zeros(3,Nmax);
+for N = 1:Nmax;
+    x = cos(pi*(0:N)'/N);
+    v = abs(x).^3; vprime = 3*x.*abs(x);
+    % 3rd deriv in BV
+    E(1,N) = norm(chebfft(v, x)-vprime,inf);
+    v = exp(-x.^(-2)); vprime = 2.*v./x.^3; % C-infinity
+    E(2,N) = norm(chebfft(v, x)-vprime,inf);
+    v = 1./(1+x.^2); vprime = -2*x.*v.^2;
+    % analytic in [-1,1]
+    E(3,N) = norm(chebfft(v, x)-vprime,inf);
+    v = x.^10; vprime = 10*x.^9;
+    % polynomial
+    E(4,N) = norm(chebfft(v, x)-vprime,inf);
+end
+toc;
+
+% Plot results:
+titles = {'|x^3|','exp(-x^{-2})','1/(1+x^2)','x^{10}'}; clf
+for iplot = 1:4
+    subplot(2,2,iplot)
+    semilogy(1:Nmax,E(iplot,:),'.','markersize',12)
+    line(1:Nmax,E(iplot,:),'linewidth',.8)
+    axis([0 Nmax 1e-16 1e3]), grid on
+    set(gca,'xtick',0:10:Nmax,'ytick',(10).^(-15:5:0))
+    xlabel N, ylabel error, title(titles(iplot))
+end
